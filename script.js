d73c96585f16d34d000fc6c0c7bf07e14d0ae952
vinay <vinkjaiswal@gmail.com>
2024-12-29 21:39:27 +0530

    Add playRound()
    Created function for one-round game calculating human and computer scores

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
 // GET human choice
 function getHumanChoice() {
     let humanChoice;
@@ -26,12 +27,38 @@ function getHumanChoice() {
         return humanChoice;
     }
     else {
-        console.log("Enter appropriate choice");
+        console.log("Did not select appropriate choice!");
     }
 }
-// GENERATE human score, computer score
 
+// GENERATE human score, computer score of one-round game
+function playRound(humanChoice, computerChoice) {
+
+    humanChoice = humanChoice.toLowerCase();
+    // conditions for human winning
+    if (humanChoice == "rock" && computerChoice == "scissors" ||
+        humanChoice == "paper" && computerChoice == "rock"  ||
+        humanChoice == "scissors" && computerChoice == "paper") {
+            humanScore = humanScore + 1;
+        }
+    // conditions for computer winning
+    else if (computerChoice == "rock" && humanChoice == "scissors" ||
+        computerChoice == "paper" && humanChoice == "rock"  ||
+        computerChoice == "scissors" && humanChoice == "paper") {
+            computerScore = computerScore + 1;
+    }
+    // when it is a draw
+    else {
+        console.log("It's a Draw!");
+    }
+}
 // SHOW who won
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(humanScore, computerScore);
