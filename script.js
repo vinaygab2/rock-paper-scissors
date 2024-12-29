de75c618d41a07e058ff1a14f48e78fcd4da59ae
vinay <vinkjaiswal@gmail.com>
2024-12-29 15:24:28 +0530

    Added getComputerChoice()
    GET computer choice as rock, paper or scissors

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // GET computer choice
+function getComputerChoice() {
+    let computerChoice;
 
+    // Generate random numbers from 0 to 2
+    computerChoice = Math.floor(Math.random()*10/3);
+    
+    if (computerChoice == 0) {
+        return "rock";
+    }
+    else if (computerChoice == 1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
 // GET human choice
 
 // GENERATE human score, computer score
 
 // SHOW who won
+
+console.log(getComputerChoice());
