710eeaa0e63520ab1192e85df3d407eb20b7af4f
vinay <vinkjaiswal@gmail.com>
2025-03-30 17:26:12 +0530

    Display score status on DOM Add divs in msg div. Display score, message on DOM. Remove console logs completely.

--- script.js
+++ script.js
@@ -18,39 +18,56 @@ function playRound(humanChoice, computerChoice) {
   // conditions for human winning
   if (humanChoice == 'rock' && computerChoice == 'scissors') {
     humanScore = humanScore + 1;
-    console.log('You win! Rock beats Scissors.');
+    winDiv.textContent = 'You win! Rock beats Scissors.';
   } else if (humanChoice == 'paper' && computerChoice == 'rock') {
     humanScore = humanScore + 1;
-    console.log('You win! Paper beats Rock.');
+    winDiv.textContent = 'You win! Paper beats Rock.';
   } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
     humanScore = humanScore + 1;
-    console.log('You win! Scissors beats Paper.');
+    winDiv.textContent = 'You win! Scissors beats Paper.';
   }
 
   // conditions for computer winning
   else if (computerChoice == 'rock' && humanChoice == 'scissors') {
     computerScore = computerScore + 1;
-    console.log('You lose! Rock beats Scissors.');
+    winDiv.textContent = 'You lose! Rock beats Scissors.';
   } else if (computerChoice == 'paper' && humanChoice == 'rock') {
     computerScore = computerScore + 1;
-    console.log('You lose! Paper beats Rock.');
+    winDiv.textContent = 'You lose! Paper beats Rock.';
   } else if (computerChoice == 'scissors' && humanChoice == 'paper') {
     computerScore = computerScore + 1;
-    console.log('You lose! Scissors beat Paper.');
+    winDiv.textContent = 'You lose! Scissors beat Paper.';
   }
-
   // when it is a draw
   else {
-    console.log("It's a Draw!");
+    winDiv.textContent = "It's a Draw";
   }
+}
 
-  console.log('Your Score: ', humanScore, 'Computer: ', computerScore);
+// SHOW who is winning
+function showStatus() {
+  scoreDiv.textContent = `You: ${humanScore} Computer: ${computerScore}`;
+
+  if (humanScore > computerScore) {
+    statusDiv.textContent = `Good! You are Ahead! (Round ${roundNo})`;
+  } else if (humanScore < computerScore) {
+    statusDiv.textContent = `Compter Ahead! (Round ${roundNo})`;
+  } else {
+    statusDiv.textContent = `Both Same! (Round ${roundNo})`;
+  }
 }
 
 let humanScore = 0;
 let computerScore = 0;
+let roundNo = 1;
 const container = document.querySelector('#container');
+const msgDiv = document.querySelector('#msg');
+const winDiv = document.querySelector('#winDiv');
+const scoreDiv = document.querySelector('#scoreDiv');
+const statusDiv = document.querySelector('#statusDiv');
 
 container.addEventListener('click', (e) => {
   playRound(e.target.id, getComputerChoice());
+  showStatus();
+  roundNo++;
 });
